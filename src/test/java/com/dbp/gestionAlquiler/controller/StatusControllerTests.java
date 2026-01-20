0c208ccddb9a9d04713b471762adf1c9db992d7e
blancoparis-tfc <blancoparis-tfc@gmail.com>
2026-01-20 22:47:36 +0100

    refactor(tests): eliminar importaciones innecesarias en StatusControllerTests

--- src/test/java/com/dbp/gestionAlquiler/controller/StatusControllerTests.java
+++ src/test/java/com/dbp/gestionAlquiler/controller/StatusControllerTests.java
@@ -5,12 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webclient.test.autoconfigure.WebClientTest;
 import org.springframework.test.web.servlet.client.RestTestClient;
-import org.springframework.boot.webclient.test.autoconfigure.AutoConfigureWebClient;
-//import org.springframework.boot.webtestclient.AutoConfigureWebTestClient;
-//import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
-//import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 
 
 
