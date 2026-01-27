8185d202d05135b26ffd89e11f49f17338c4dc42
blancoparis-tfc <blancoparis-tfc@gmail.com>
2026-01-27 23:36:27 +0100

    feat(test): mejorar pruebas de integración del StatusController con configuración de MockMvc

--- src/test/java/com/dbp/gestionAlquiler/controller/StatusControllerIntegrationTest.java
+++ src/test/java/com/dbp/gestionAlquiler/controller/StatusControllerIntegrationTest.java
@@ -1,19 +1,28 @@
 package com.dbp.gestionAlquiler.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-
 public class StatusControllerIntegrationTest {
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
     private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
     public void testStatusControllerIsAvailable() throws Exception {
